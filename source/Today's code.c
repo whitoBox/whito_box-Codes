8063fdc4cff22c8115e633344b423024b5d2c633
whitoBox <163735623+whitoBox@users.noreply.github.com>
2024-03-19 17:14:01 +0900

    Update and rename Today's code to Today's code.c

--- source/Today's code.c
+++ source/Today's code.c
@@ -1,6 +1,6 @@
 ```c
 #include <stdio.h>
-int main(void)
+int main(void) 
 {
     printf("|%d|",358);
     printf(": 정수형 기본 자릿수\n");
@@ -9,7 +9,7 @@ int main(void)
     printf(": 전체 자릿수 5개\n");
 
     printf("|%05d|",358);
-    printf(": 전체 자릿수는 5개이고 빈칸은 0으로 채움\n");
+    printf(": 전체 자릿수는 5개이고 빈칸은 0으로 채움.\n");
    
     return 0;
 }
