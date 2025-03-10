b874f319c2156a97dfb67f00da29a061bfc57840
John <kimyohan828@gmail.com>
2025-03-10 10:35:11 +0900

    2 semester stuff

---
+++ Java_Codes/week01/src/week01/Helloworld.java
@@ -0,0 +1,9 @@
+package week01;
+
+public class Helloworld {
+	public static void main(String[] args) {
+		System.out.println("학과 : 컴퓨터정보공학과");
+		System.out.println("학번 : 202444029");
+		System.out.println("이름 : 김요한");
+	}
+}
