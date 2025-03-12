dbf315d8645336da3f7babd8c35de9d3dacba9a3
John <kimyohan828@gmail.com>
2025-03-12 16:12:57 +0900

    C# week02

---
+++ C#_Codes/Week02/Week02Proj01/FromMain.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Week02Proj01
+{
+    partial class FromMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxInput1 = new System.Windows.Forms.TextBox();
+            this.tbxInput2 = new System.Windows.Forms.TextBox();
+            this.tbxInput3 = new System.Windows.Forms.TextBox();
+            this.tbxInput4 = new System.Windows.Forms.TextBox();
+            this.tbxInput5 = new System.Windows.Forms.TextBox();
+            this.chkToggle = new System.Windows.Forms.CheckBox();
+            this.btnOutput01 = new System.Windows.Forms.Button();
+            this.btnOutput02 = new System.Windows.Forms.Button();
+            this.btnOutput03 = new System.Windows.Forms.Button();
+            this.btnOutput04 = new System.Windows.Forms.Button();
+            this.btnOutput05 = new System.Windows.Forms.Button();
+            this.btnOutput06 = new System.Windows.Forms.Button();
+            this.btnOutput08 = new System.Windows.Forms.Button();
+            this.btnOutput07 = new System.Windows.Forms.Button();
+            this.btnOutput09 = new System.Windows.Forms.Button();
+            this.btnOutput10 = new System.Windows.Forms.Button();
+            this.btnOutput11 = new System.Windows.Forms.Button();
+            this.btnOutput13 = new System.Windows.Forms.Button();
+            this.btnOutput12 = new System.Windows.Forms.Button();
+            this.btnOutput14 = new System.Windows.Forms.Button();
+            this.btnOutput15 = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbxInput1
+            // 
+            this.tbxInput1.Location = new System.Drawing.Point(13, 13);
+            this.tbxInput1.Name = "tbxInput1";
+            this.tbxInput1.Size = new System.Drawing.Size(100, 21);
+            this.tbxInput1.TabIndex = 0;
+            // 
+            // tbxInput2
+            // 
+            this.tbxInput2.Location = new System.Drawing.Point(119, 13);
+            this.tbxInput2.Name = "tbxInput2";
+            this.tbxInput2.Size = new System.Drawing.Size(100, 21);
+            this.tbxInput2.TabIndex = 0;
+            // 
+            // tbxInput3
+            // 
+            this.tbxInput3.Location = new System.Drawing.Point(227, 13);
+            this.tbxInput3.Name = "tbxInput3";
+            this.tbxInput3.Size = new System.Drawing.Size(100, 21);
+            this.tbxInput3.TabIndex = 0;
+            // 
+            // tbxInput4
+            // 
+            this.tbxInput4.Location = new System.Drawing.Point(333, 13);
+            this.tbxInput4.Name = "tbxInput4";
+            this.tbxInput4.Size = new System.Drawing.Size(100, 21);
+            this.tbxInput4.TabIndex = 0;
+            // 
+            // tbxInput5
+            // 
+            this.tbxInput5.Location = new System.Drawing.Point(439, 13);
+            this.tbxInput5.Name = "tbxInput5";
+            this.tbxInput5.Size = new System.Drawing.Size(100, 21);
+            this.tbxInput5.TabIndex = 0;
+            // 
+            // chkToggle
+            // 
+            this.chkToggle.AutoSize = true;
+            this.chkToggle.Location = new System.Drawing.Point(546, 17);
+            this.chkToggle.Name = "chkToggle";
+            this.chkToggle.Size = new System.Drawing.Size(15, 14);
+            this.chkToggle.TabIndex = 1;
+            this.chkToggle.UseVisualStyleBackColor = true;
+            // 
+            // btnOutput01
+            // 
+            this.btnOutput01.Location = new System.Drawing.Point(13, 41);
+            this.btnOutput01.Name = "btnOutput01";
+            this.btnOutput01.Size = new System.Drawing.Size(100, 25);
+            this.btnOutput01.TabIndex = 2;
+            this.btnOutput01.Text = "가감1";
+            this.btnOutput01.UseVisualStyleBackColor = true;
+            this.btnOutput01.Click += new System.EventHandler(this.btnOutput01_Click);
+            // 
+            // btnOutput02
+            // 
+            this.btnOutput02.Location = new System.Drawing.Point(119, 41);
+            this.btnOutput02.Name = "btnOutput02";
+            this.btnOutput02.Size = new System.Drawing.Size(100, 25);
+            this.btnOutput02.TabIndex = 2;
+            this.btnOutput02.Text = "가감2";
+            this.btnOutput02.UseVisualStyleBackColor = true;
+            this.btnOutput02.Click += new System.EventHandler(this.btnOutput02_Click);
+            // 
+            // btnOutput03
+            // 
+            this.btnOutput03.Location = new System.Drawing.Point(225, 41);
+            this.btnOutput03.Name = "btnOutput03";
+            this.btnOutput03.Size = new System.Drawing.Size(100, 25);
+            this.btnOutput03.TabIndex = 2;
+            this.btnOutput03.Text = "가감3";
+            this.btnOutput03.UseVisualStyleBackColor = true;
+            this.btnOutput03.Click += new System.EventHandler(this.btnOutput03_Click);
+            // 
+            // btnOutput04
+            // 
+            this.btnOutput04.Location = new System.Drawing.Point(331, 41);
+            this.btnOutput04.Name = "btnOutput04";
+            this.btnOutput04.Size = new System.Drawing.Size(100, 25);
+            this.btnOutput04.TabIndex = 2;
+            this.btnOutput04.Text = "가감4";
+            this.btnOutput04.UseVisualStyleBackColor = true;
+            this.btnOutput04.Click += new System.EventHandler(this.btnOutput04_Click);
+            // 
+            // btnOutput05
+            // 
+            this.btnOutput05.Location = new System.Drawing.Point(437, 41);
+            this.btnOutput05.Name = "btnOutput05";
+            this.btnOutput05.Size = new System.Drawing.Size(100, 25);
+            this.btnOutput05.TabIndex = 2;
+            this.btnOutput05.Text = "문자와 문자열";
+            this.btnOutput05.UseVisualStyleBackColor = true;
+            this.btnOutput05.Click += new System.EventHandler(this.btnOutput05_Click);
+            // 
+            // btnOutput06
+            // 
+            this.btnOutput06.Location = new System.Drawing.Point(13, 72);
+            this.btnOutput06.Name = "btnOutput06";
+            this.btnOutput06.Size = new System.Drawing.Size(100, 25);
+            this.btnOutput06.TabIndex = 2;
+            this.btnOutput06.Text = "형변환";
+            this.btnOutput06.UseVisualStyleBackColor = true;
+            this.btnOutput06.Click += new System.EventHandler(this.btnOutput06_Click);
+            // 
+            // btnOutput08
+            // 
+            this.btnOutput08.Location = new System.Drawing.Point(225, 72);
+            this.btnOutput08.Name = "btnOutput08";
+            this.btnOutput08.Size = new System.Drawing.Size(100, 25);
+            this.btnOutput08.TabIndex = 2;
+            this.btnOutput08.UseVisualStyleBackColor = true;
+            // 
+            // btnOutput07
+            // 
+            this.btnOutput07.Location = new System.Drawing.Point(119, 72);
+            this.btnOutput07.Name = "btnOutput07";
+            this.btnOutput07.Size = new System.Drawing.Size(100, 25);
+            this.btnOutput07.TabIndex = 2;
+            this.btnOutput07.UseVisualStyleBackColor = true;
+            // 
+            // btnOutput09
+            // 
+            this.btnOutput09.Location = new System.Drawing.Point(331, 72);
+            this.btnOutput09.Name = "btnOutput09";
+            this.btnOutput09.Size = new System.Drawing.Size(100, 25);
+            this.btnOutput09.TabIndex = 2;
+            this.btnOutput09.UseVisualStyleBackColor = true;
+            // 
+            // btnOutput10
+            // 
+            this.btnOutput10.Location = new System.Drawing.Point(437, 72);
+            this.btnOutput10.Name = "btnOutput10";
+            this.btnOutput10.Size = new System.Drawing.Size(100, 25);
+            this.btnOutput10.TabIndex = 2;
+            this.btnOutput10.UseVisualStyleBackColor = true;
+            // 
+            // btnOutput11
+            // 
+            this.btnOutput11.Location = new System.Drawing.Point(13, 103);
+            this.btnOutput11.Name = "btnOutput11";
+            this.btnOutput11.Size = new System.Drawing.Size(100, 25);
+            this.btnOutput11.TabIndex = 2;
+            this.btnOutput11.UseVisualStyleBackColor = true;
+            // 
+            // btnOutput13
+            // 
+            this.btnOutput13.Location = new System.Drawing.Point(225, 103);
+            this.btnOutput13.Name = "btnOutput13";
+            this.btnOutput13.Size = new System.Drawing.Size(100, 25);
+            this.btnOutput13.TabIndex = 2;
+            this.btnOutput13.UseVisualStyleBackColor = true;
+            // 
+            // btnOutput12
+            // 
+            this.btnOutput12.Location = new System.Drawing.Point(119, 103);
+            this.btnOutput12.Name = "btnOutput12";
+            this.btnOutput12.Size = new System.Drawing.Size(100, 25);
+            this.btnOutput12.TabIndex = 2;
+            this.btnOutput12.UseVisualStyleBackColor = true;
+            // 
+            // btnOutput14
+            // 
+            this.btnOutput14.Location = new System.Drawing.Point(331, 103);
+            this.btnOutput14.Name = "btnOutput14";
+            this.btnOutput14.Size = new System.Drawing.Size(100, 25);
+            this.btnOutput14.TabIndex = 2;
+            this.btnOutput14.UseVisualStyleBackColor = true;
+            // 
+            // btnOutput15
+            // 
+            this.btnOutput15.Location = new System.Drawing.Point(437, 103);
+            this.btnOutput15.Name = "btnOutput15";
+            this.btnOutput15.Size = new System.Drawing.Size(100, 25);
+            this.btnOutput15.TabIndex = 2;
+            this.btnOutput15.UseVisualStyleBackColor = true;
+            // 
+            // lblResult
+            // 
+            this.lblResult.BackColor = System.Drawing.Color.White;
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResult.Location = new System.Drawing.Point(13, 135);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(524, 257);
+            this.lblResult.TabIndex = 3;
+            // 
+            // FromMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnOutput15);
+            this.Controls.Add(this.btnOutput14);
+            this.Controls.Add(this.btnOutput10);
+            this.Controls.Add(this.btnOutput09);
+            this.Controls.Add(this.btnOutput12);
+            this.Controls.Add(this.btnOutput05);
+            this.Controls.Add(this.btnOutput07);
+            this.Controls.Add(this.btnOutput13);
+            this.Controls.Add(this.btnOutput04);
+            this.Controls.Add(this.btnOutput08);
+            this.Controls.Add(this.btnOutput11);
+            this.Controls.Add(this.btnOutput02);
+            this.Controls.Add(this.btnOutput06);
+            this.Controls.Add(this.btnOutput03);
+            this.Controls.Add(this.btnOutput01);
+            this.Controls.Add(this.chkToggle);
+            this.Controls.Add(this.tbxInput5);
+            this.Controls.Add(this.tbxInput4);
+            this.Controls.Add(this.tbxInput3);
+            this.Controls.Add(this.tbxInput2);
+            this.Controls.Add(this.tbxInput1);
+            this.Name = "FromMain";
+            this.Text = "2주차";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxInput1;
+        private System.Windows.Forms.TextBox tbxInput2;
+        private System.Windows.Forms.TextBox tbxInput3;
+        private System.Windows.Forms.TextBox tbxInput4;
+        private System.Windows.Forms.TextBox tbxInput5;
+        private System.Windows.Forms.CheckBox chkToggle;
+        private System.Windows.Forms.Button btnOutput01;
+        private System.Windows.Forms.Button btnOutput02;
+        private System.Windows.Forms.Button btnOutput03;
+        private System.Windows.Forms.Button btnOutput04;
+        private System.Windows.Forms.Button btnOutput05;
+        private System.Windows.Forms.Button btnOutput06;
+        private System.Windows.Forms.Button btnOutput08;
+        private System.Windows.Forms.Button btnOutput07;
+        private System.Windows.Forms.Button btnOutput09;
+        private System.Windows.Forms.Button btnOutput10;
+        private System.Windows.Forms.Button btnOutput11;
+        private System.Windows.Forms.Button btnOutput13;
+        private System.Windows.Forms.Button btnOutput12;
+        private System.Windows.Forms.Button btnOutput14;
+        private System.Windows.Forms.Button btnOutput15;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ C#_Codes/Week02/Week02Proj01/FromMain.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week02Proj01
+{
+    public partial class FromMain: Form
+    {
+        public FromMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOutput01_Click(object sender, EventArgs e)
+        {
+            bool isToggle = chkToggle.Checked; // true or false
+            if (isToggle) {
+                string data1 = tbxInput1.Text;
+                string data2 = tbxInput2.Text;
+                string result = data1 + data2; //문자열 연결 연산자
+                lblResult.Text = result;
+            }
+            else {
+                int data1 = int.Parse(tbxInput1.Text);
+                int data2 = int.Parse(tbxInput2.Text);
+                int result = data1 + data2; //산술 연산자
+                lblResult.Text = result.ToString();//모든 값을 문자열로 바꿀수 있임
+            }
+        }
+
+        private void btnOutput02_Click(object sender, EventArgs e)
+        {
+            if (chkToggle.Checked == false)
+            {
+                int data1 = int.Parse(tbxInput1.Text);
+                int data2 = int.Parse(tbxInput2.Text);
+                int result = data1 + data2; //산술 연산자
+                lblResult.Text = "더하기:" + result.ToString();
+            }
+            else
+            {
+                int data1 = int.Parse(tbxInput1.Text);
+                int data2 = int.Parse(tbxInput2.Text);
+                int result = data1 - data2; //산술 연산자
+                lblResult.Text = "빼기:" + result; //문자열+숫자 = 문자열
+            }
+        }
+
+        private void btnOutput03_Click(object sender, EventArgs e)
+        {
+            int data1 = int.Parse(tbxInput1.Text);
+            int data2 = int.Parse(tbxInput2.Text);
+            if (chkToggle.Checked == false)
+            {
+                int result = data1 + data2; //산술 연산자
+                lblResult.Text = string.Format("더하기:{0}", result);
+            }
+            else
+            {
+                int result = data1 - data2; //산술 연산자
+                lblResult.Text = $"뺴기: {result}"; //문자열 보간법
+            }
+        }
+
+        private void btnOutput04_Click(object sender, EventArgs e)
+        {
+            double data1 = double.Parse(tbxInput1.Text);
+            double data2 = double.Parse(tbxInput2.Text);
+            if (chkToggle.Checked == false)
+            {
+                double result = data1 + data2; //산술 연산자
+                lblResult.Text = string.Format("더하기:{0}", result);
+            }
+            else
+            {
+                double result = data1 - data2; //산술 연산자
+                lblResult.Text = $"뺴기: {result}"; //문자열 보간법
+            }
+        }
+
+        private void btnOutput05_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = tbxInput1.Text;
+            lblResult.Text += Environment.NewLine; // "\r\n" string 타입이다
+            //lblResult.Text = Environment.NewLine; // 문자열
+            lblResult.Text += tbxInput1.Text.GetType();
+            //lblResult.Text = tbxInput1.Text.GetType(); Type 자료형 즉 대입이 안된다
+            lblResult.Text += Environment.NewLine;
+
+            lblResult.Text += tbxInput1.Text[0]; //문자
+            //lblResult.Text = tbxInput1.Text[0]; 문자 + 문자열 =/ 안된다.
+            lblResult.Text += Environment.NewLine;
+            lblResult.Text += tbxInput1.Text[0].GetType();
+
+            lblResult.Text += Environment.NewLine;
+            char test1 = tbxInput1.Text[0];
+            //c언어 char 1바이트 (ascii)
+            //c# char 2바이트 (unicode)
+            byte result1 = (byte)test1;   //1바이트 부호미지원 정수형
+            sbyte result4 = (sbyte)test1; //1바이트 부호지원 정수형
+            short result2 = (byte)test1;  //2바이트 부호지원 정수형
+            ushort result3 = test1;       //2바이트 부호미지원 정수형
+            //int, uint, long, ulong(8바이트) u = unsigned
+            lblResult.Text +=
+                $"{test1},{result1},{result2},{result3}";
+
+        }
+
+        private void btnOutput06_Click(object sender, EventArgs e)
+        {   
+            //1) 정수 -> 실수: OK
+            //2) 실수 -> 정수: 처리 필요
+            //3) 작은 숫자 -> 큰 숫자 : OK
+            //4) 큰 숫자 -> 작은 숫자 : 처리 필요
+            int data1 = short.Parse(tbxInput1.Text); //3번
+            float data2 = (float) double.Parse(tbxInput2.Text); //4번
+            long data3 = long.Parse(tbxInput3.Text);
+            int data4 = (int)data3; //4번
+
+            double result1 = data1 + data2 + data3 + data4;
+            lblResult.Text = result1.ToString();
+
+            lblResult.Text += "\r\n";
+            lblResult.Text += "\n";
+
+            // (int)1.9 + (int)1.6 => 2
+            long result2 = data1 + (long)data2 + data3 + data4;
+            lblResult.Text += result2.ToString();
+
+            lblResult.Text += "\r\n";
+            lblResult.Text += "\n";
+            //(int) (1.9 + 1.6) => 3
+            long result3 = (long)(data1 + data2 + data3 + data4);
+            lblResult.Text += result3.ToString();
+        }
+    }
+}
