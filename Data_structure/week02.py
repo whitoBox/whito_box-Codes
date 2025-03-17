dcc3e76d8eb24983ad90115809a6a7d2dd04c38d
John <kimyohan828@gmail.com>
2025-03-17 11:43:48 +0900

    ds week02 0(1)

--- Data_structure/week02.py
+++ Data_structure/week02.py
@@ -1,6 +1,5 @@
-num = int(input("10단위 수 입력 : "))
-summ = 0
+num = int(input("10단위 수 입력 : "))g
 #for i in range(1, num+1):
 #    summ += i
-summ = num * (num+1) // 2
+summ = num * (num+1) // 2 #O(1)
 print(summ)
