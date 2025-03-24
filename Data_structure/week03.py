b06cb7e8821157fde1faa3d40aad71dcbe7b34bc
John <kimyohan828@gmail.com>
2025-03-24 11:57:46 +0900

    week 04 array set 07

--- Data_structure/week03.py
+++ Data_structure/week03.py
@@ -1,3 +1,15 @@
+def dups(city):
+    result = []
+    a = set()
+
+    for v in city:
+        l1 = len(a)
+        a.add(v)
+        l2 = len(a)
+        if l1 == l2: #증복 값이 들어옹
+            result.append(v)
+    return result
 city = ['a','a','b','asf']
-city = set(city)
+#city = set(city)
 print(city)
+print(dups(city))
