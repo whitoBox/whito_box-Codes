3fc847e9f1a6800042d22d9a6dab0484177f550b
John <kimyohan828@gmail.com>
2025-03-24 10:45:29 +0900

    week4 array

--- Data_structure/O(log_n).py
+++ Data_structure/O(log_n).py
@@ -3,7 +3,7 @@ guessnum = random.randint(1,100)
 count = 0
 while(True):
     count += 1
-    player = int(input("1-100 사이 값을 맞추시오"))
+    player = int(input("1-100 사이 값을 맞추시오: "))
     if player == guessnum:
         print(f"attempt :{count}")
         print("correct!")
---
+++ Data_structure/week03.py
@@ -0,0 +1,3 @@
+array = [11, 9, -77, 8]
+for i in array:
+    print(i,id(i))
