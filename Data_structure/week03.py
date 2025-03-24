e30308fe344363340ac75be21bba276b1a3aef20
John <kimyohan828@gmail.com>
2025-03-24 11:29:48 +0900

    week04 array 04

--- Data_structure/week03.py
+++ Data_structure/week03.py
@@ -1,5 +1,13 @@
-import array
+def mv_zeros(a_list):
+    zindex = 0
+    for index, n in enumerate(a_list):
+        if n != 0:
+            a_list[zindex] = n
+            if zindex != index:
+                a_list[index] = 0
+            zindex += 1
+    return (a_list)
 
-array = array.array('f', [11,9, -77, 8])
-for i in range(len(array)):
-    print(f"{array[i]:6} {id(i)}")
+lst = [8, 0, 3, 0, 12]
+mv_zeros(lst)
+print(lst)=====
