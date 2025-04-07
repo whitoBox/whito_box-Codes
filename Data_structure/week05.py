ae8c51493cf64e66250fbc64a2526e4d50de89f9
John <kimyohan828@gmail.com>
2025-04-07 11:44:19 +0900

    DS week05 stack with list v1.0

--- Data_structure/week05.py
+++ Data_structure/week05.py
@@ -1,33 +1,11 @@
-class Node:
-    def __init__(self,data):
-        self.data = data
-        self.link = None
+s1 = list()
+s1.append("dhfs")
+s1.append("dhfDs")
+s1.append("dhfDEs")
 
-class Stack:
-    def __init__(self):
-        self.top = None
-
-    def push(self,data):
-        node = Node(data)
-        if self.top is None:
-            self.top = node
-        else:
-            node.next = self.top
-            self.top = node
-
-    def pop(self):
-        if self.top is None:
-            return 'Error: Stack is empty'
-        popped_node = self.top
-        self.top = self.top.link
-        return f"popped: ({popped_node.data})"
-
-s1 = Stack()
-#print(s1.pop())
-s1.push("data_structure")
-s1.push("database")
-s1.push("thing")
-#print(s1.pop())
-#print(s1.pop())
-for i in range(2):
-    print(s1.pop())
+print(len(s1))
+print(s1)
+print(s1.pop())
+print(s1)
+print(s1.pop())
+print(s1)
