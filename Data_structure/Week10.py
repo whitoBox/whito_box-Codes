a4d60eac5a0173434b6d3596d143f87671fc2dc8
John <kimyohan828@gmail.com>
2025-05-12 10:31:26 +0900

    week10 Serch

--- Data_structure/Week10.py
+++ Data_structure/Week10.py
@@ -45,6 +45,23 @@ def insert(root, value):
             current = current.right  # move
     return root
 
+def serch():
+    find_number = int(input("찾으려는 값:"))
+    current = root
+    while True:
+        if find_number == current.data:
+            print(f"{find_number}을(를) 찾았습니다")
+            break
+        elif find_number < current.data:
+            if current.left is None:
+                print(f"{find_number}이(가) 존재하지 않습니다")
+                break
+            current = current.left
+        else:
+            if current.right is None:
+                print(f"{find_number}이(가) 존재하지 않습니다")
+                break
+            current = current.right
 
 if __name__ == "__main__":
     numbers = [10, 15,9, 8, 3, 9,11,9,12,9]
@@ -55,22 +72,6 @@ if __name__ == "__main__":
 
     print("BST 구성 완료")
     post_order(root)  # 3 9 8 15 10
+    print()
+    serch()
 
-
-    # find_number = int(input())
-    #
-    # current = root
-    # while True:
-    #     if find_number == current.data:
-    #         print(f"{find_number}을(를) 찾았습니다")
-    #         break
-    #     elif find_number < current.data:
-    #         if current.left is None:
-    #             print(f"{find_number}이(가) 존재하지 않습니다")
-    #             break
-    #         current = current.left
-    #     else:
-    #         if current.right is None:
-    #             print(f"{find_number}이(가) 존재하지 않습니다")
-    #             break
-    #         current = current.righ
