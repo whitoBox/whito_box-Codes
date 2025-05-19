7e1c3a46fc2f1d05f9a786212049a2ec33b8bd66
John <kimyohan828@gmail.com>
2025-05-19 12:03:39 +0900

    graph bfs

--- Data_structure/Week11.py
+++ Data_structure/Week11.py
@@ -1,42 +1,35 @@
-class Graph:
-    def __init__(self, size):
-        self.size = size
-        self.graph = [[0 for _ in range(size)] for _ in range(size)]
+graph = [
+    [0, 1, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 0, 1, 1, 1, 0],
+    [0, 0, 0, 1, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1, 0]
+]
+graph2 = [
+    [0, 0, 1, 1, 0, 0, 0, 0],  # A–C, A–D
+    [0, 0, 1, 0, 0, 0, 0, 0],  # B–C
+    [1, 1, 0, 0, 0, 0, 0, 0],  # C–A, C–B
+    [1, 0, 0, 0, 1, 0, 0, 0],  # D–A, D–E
+    [0, 0, 0, 1, 0, 1, 0, 0],  # E–D, E–F
+    [0, 0, 0, 0, 1, 0, 1, 0],  # F–E, F–G
+    [0, 0, 0, 0, 0, 1, 0, 1],  # G–F, G–H
+    [0, 0, 0, 0, 0, 0, 1, 0],  # H–G
+]
+def dfs (g,i,visited):
+    visited[i] = 1
+    print(chr(ord('A')+i), end='')
+    for j in range(len(g)):
+        if g[i][j] == 1 and not visited[j]:
+            dfs(g, j, visited)
 
+def bfs(g,i, visited):
+    pass
 
-G1 = Graph(4)
-G3 = Graph(4)
-G_self = Graph(4)
-
-# 0 == A, 1 == B, 2 == C, 3 == D
-G1.graph[0][1] = 1; G1.graph[0][2] = 1; G1.graph[0][3] = 1
-G1.graph[1][0] = 1; G1.graph[1][2] = 1
-G1.graph[2][0] = 1; G1.graph[2][1] = 1; G1.graph[2][3] = 1
-G1.graph[3][0] = 1; G1.graph[3][2] = 1
-
-print("G1 무방향 그래프")
-for r in range(G1.size):
-    for c in range(G1.size):
-        print(G1.graph[r][c], end=' ')
-    print()
-
-G3.graph[0][1] = 1; G3.graph[0][2] = 1
-G3.graph[3][0] = 1; G3.graph[3][2] = 1
-
-print("G3 방향 그래프")
-for r in range(G3.size):
-    for c in range(G3.size):
-        print(G3.graph[r][c], end=' ')
-    print()
-
-# 0 == A, 1 == B, 2 == C, 3 == D
-G_self.graph[0][3] = 1
-G_self.graph[1][2] = 1; G_self.graph[1][3] = 1
-G_self.graph[2][1] = 1
-G_self.graph[3][0] = 1; G_self.graph[3][1] = 1
-
-print("G_self 무방향 그래프")
-for r in range(G_self.size):
-    for c in range(G_self.size):
-        print(G_self.graph[r][c], end=' ')
-    print()
+visited_dfs = [0 for _ in range(len(graph2))]
+visited_bfs = [0 for _ in range(len(graph2))]
+dfs(graph, 2, visited_dfs)
+print()
+bfs(graph, 0, visited_dfs)
