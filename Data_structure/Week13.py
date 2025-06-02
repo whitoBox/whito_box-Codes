8632d85037b4a5e2584e8ba87dd3328d4d12339c
John <kimyohan828@gmail.com>
2025-06-02 12:19:04 +0900

    graph Kruskal Algorithm

--- Data_structure/Week13.py
+++ Data_structure/Week13.py
@@ -2,6 +2,25 @@ class Graph:
 	def __init__ (self, size):
 		self.graph = [[0 for _ in range(size)] for _ in range(size)]
 
+
+class DisjointSet:  # 크루스칼 알고리즘을 위한 유틸리티 클래스
+	def __init__(self, n):
+		self.parent = [i for i in range(n)]
+
+	def find(self, x):
+		if self.parent[x] != x:
+			self.parent[x] = self.find(self.parent[x])
+		return self.parent[x]
+
+	def union(self, x, y):
+		x_root = self.find(x)
+		y_root = self.find(y)
+		if x_root != y_root:
+			self.parent[y_root] = x_root
+			return True
+		return False
+
+
 def print_graph(g) :
 	print(' ', end = ' ')
 	for v in range(len(g.graph)) :
@@ -14,48 +33,17 @@ def print_graph(g) :
 		print()
 	print()
 
-def find_vertex(g, find_vtx) :
-	stack = list()
-	visited_ary = list()
-
-	current = 0	# 시작 정점
-	stack.append(current)
-	visited_ary.append(current)
-
-	while stack:
-		next = None
-		for vertex in range(graph_size):
-			if g.graph[current][vertex] != 0:  # 연결되어 있으면
-				if vertex in visited_ary:	# 방문한 적이 있는 정점
-					pass
-				else :			# 방문한 적이 없으면
-					next = vertex  #  다음 정점으로 지정
-					break
-
-		if next is not None:				# 다음에 방문할 정점이 있는 경우
-			current = next
-			stack.append(current)  # push
-			visited_ary.append(current)  # push
-		else :					# 다음에 방문할 정점이 없는 경우
-			current = stack.pop()
-
-	if find_vtx in visited_ary:
-		return True
-	else :
-		return False
-
 
 g1 = None
 name_ary = ['인천', '서울', '강릉', '대전', '광주', '부산']
-chuncheon, seoul, sokcho, daejeon, gwangju, busan = 0, 1, 2, 3, 4, 5
-
+incheon, seoul, gangneung, daejeon, gwangju, busan = 0, 1, 2, 3, 4, 5
 
 graph_size = 6
 g1 = Graph(graph_size)
-g1.graph[chuncheon][seoul] = 10; g1.graph[chuncheon][sokcho] = 15
-g1.graph[seoul][chuncheon] = 10; g1.graph[seoul][sokcho] = 40; g1.graph[seoul][daejeon] = 11; g1.graph[seoul][gwangju] = 55
-g1.graph[sokcho][chuncheon] = 15; g1.graph[sokcho][seoul] = 40; g1.graph[sokcho][daejeon] = 12
-g1.graph[daejeon][seoul] = 11; g1.graph[daejeon][sokcho] = 12; g1.graph[daejeon][gwangju] = 20; g1.graph[daejeon][busan] = 30
+g1.graph[incheon][seoul] = 10; g1.graph[incheon][gangneung] = 15
+g1.graph[seoul][incheon] = 10; g1.graph[seoul][gangneung] = 40; g1.graph[seoul][daejeon] = 11; g1.graph[seoul][gwangju] = 55
+g1.graph[gangneung][incheon] = 15; g1.graph[gangneung][seoul] = 40; g1.graph[gangneung][daejeon] = 12
+g1.graph[daejeon][seoul] = 11; g1.graph[daejeon][gangneung] = 12; g1.graph[daejeon][gwangju] = 20; g1.graph[daejeon][busan] = 30
 g1.graph[gwangju][seoul] = 55; g1.graph[gwangju][daejeon] = 20; g1.graph[gwangju][busan] = 28
 g1.graph[busan][daejeon] = 30; g1.graph[busan][gwangju] = 28
 
@@ -64,48 +52,31 @@ print_graph(g1)
 
 edge_ary = []
 for i in range(graph_size) :
-	for k in range(graph_size) :
-		if g1.graph[i][k] != 0 :
-			edge_ary.append([g1.graph[i][k], i, k])
+	for j in range(graph_size) :
+		if g1.graph[i][j] != 0 :
+			edge_ary.append([g1.graph[i][j], i, j])
 print(edge_ary)
 
-edge_ary.sort(reverse=True)
+edge_ary.sort()  # 오름차순
 print(edge_ary)
 
-new_ary = list()
-for i in range(1, len(edge_ary), 2):
-	new_ary.append(edge_ary[i])
-print(new_ary)
 
-index = 0
-while len(new_ary) > graph_size - 1:
-	start = new_ary[index][1]
-	end = new_ary[index][2]
-	save_cost = new_ary[index][0]
+ds = DisjointSet(graph_size)
+mst_edges = list()
+mst_cost = 0
 
-	g1.graph[start][end] = 0
-	g1.graph[end][start] = 0
-
-	start_reachable = find_vertex(g1, start)
-	end_reachable = find_vertex(g1, end)
-
-	if start_reachable and end_reachable :
-		del new_ary[index]
-	else:
-		g1.graph[start][end] = save_cost
-		g1.graph[end][start] = save_cost
-		index = index + 1
+for cost, s, e in edge_ary:
+	if ds.union(s, e):
+		mst_edges.append((cost, s, e))
+		mst_cost = mst_cost + cost
 
 
+mst_graph = Graph(graph_size)
+for cost, s, e in mst_edges:
+	mst_graph.graph[s][e] = cost
+	mst_graph.graph[e][s] = cost
 
-print('최소 비용 연결도')
-print_graph(g1)
 
-total_cost = 0
-for i in range(graph_size):
-	for j in range(graph_size):
-		if g1.graph[i][j] != 0:
-			total_cost = total_cost + g1.graph[i][j]
-
-total_cost = total_cost // 2
-print(f"최소 비용의 도로 건설 비용 :  {total_cost}")
+print('최소 비용 계산')
+print_graph(mst_graph)
+print(f"최소 비용 :  {mst_cost}")
