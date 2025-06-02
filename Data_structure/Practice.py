0b774b559cb78b4581b1f1eace4249ef2893a216
John <kimyohan828@gmail.com>
2025-06-02 11:09:39 +0900

    ds week13 new graph code

--- Data_structure/Practice.py
+++ Data_structure/Practice.py
@@ -1,53 +1,45 @@
-class tree_node():
-    def __init__(self,data=None):
-        self.data = data
-        self.left = None
-        self.right = None
+def is_connected (te, v) -> bool:
+    pass
 
-def insert(root,data):
-    data = tree_node(data)
-    current = root
-    if root.data is None:
-        root = data
-        return root
+v,e = map(int, input().split()) #정점 ,간선 입력
+edges = list()
 
-    while True:
-        if data.data > current.data:
-            if current.right is None:
-                current.right = data
-                break
-            current = current.right
+for _ in range(e):
+    a,b,cost = map(int, input().split())
+    edges.append((cost,a,b))
 
-        else:
-            if current.left is None:
-                current.left = data
-                break
-            current = current.left
-    return root
+print(edges)
+edges.sort(reverse=True)
+print(edges)
 
-def preOrder(node):
-    if node:
-        print(node.data,end='-')
-        preOrder(node.left)
-        preOrder(node.right)
-def postOrder(node):
-    if node:
-        postOrder(node.left)
-        postOrder(node.right)
-        print(node.data,end='-')
-def inOrder(node):
-    if node:
-        inOrder(node.left)
-        print(node.data,end='-')
-        inOrder(node.right)
+selected_edges = edges[:]
 
-tree = tree_node()
-nums = [5,51,6,8,48,100]
-for num in nums:
-    tree = insert(tree,num)
+total_cost = sum(cost for cost,a,b in edges)
 
-preOrder(tree)
-print()
-inOrder(tree)
-print()
-postOrder(tree)
+
+for cost,a,b in edges:
+    temp_edges = [(c,x,y) for c,x,y in selected_edges]
+    if is_connected(temp_edges, v):
+        selected_edges = temp_edges
+        total_cost = total_cost - cost
+        print(f"간선 ({a}----{b}, 가중치 : {cost}) 제거, 현 시점 촘 가중치: ({total_cost})")
+    else:
+        print(f"간선 ({a}----{b}, 가중치 : {cost}) 유지: (제거하면 연결 끊어짐)")
+
+print(f"\n 최소 신장 트리의 총 가중치: {total_cost}")
+
+for cost,a,b in sorted(selected_edges):
+    print(f"{a}----{b}, {cost}")
+
+'''
+6 9
+0 1 10
+0 2 15
+1 2 40
+1 3 11
+1 4 50
+2 3 12
+3 4 20
+3 5 30
+4 5 25
+'''
--- Data_structure/Week12.py
+++ Data_structure/Week12.py
@@ -1,37 +1,44 @@
-from collections import deque
+def is_connected (te, v) -> bool:
+    pass
 
-graph = [
-    [0, 1, 1, 0, 0, 0, 0, 0],
-    [1, 0, 0, 1, 0, 0, 0, 0],
-    [1, 0, 0, 1, 0, 0, 0, 0],
-    [0, 1, 1, 0, 1, 1, 1, 0],
-    [0, 0, 0, 1, 0, 1, 0, 0],
-    [0, 0, 0, 1, 1, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 1, 0]
-]
+v,e = map(int, input().split()) #정점 ,간선 입력
+edges = list()
 
-def dfs (g,i,visited):
-    visited[i] = 1
-    print(chr(ord('A')+i), end=' ')
-    for j in range(len(g)):
-        if g[i][j] == 1 and not visited[j]:
-            dfs(g, j, visited)
+for _ in range(e):
+    a,b,cost = map(int, input().split())
+    edges.append((cost,a,b))
 
-def bfs(g,i, visited):
-    queue = deque([i]) #popleft,append
-    visited[i] = 1
-    while queue:
-        #print(visited)
-        i = queue.popleft() #dequeue
-        print(chr(ord('A') + i), end=' ')
-        for j in range(len(g)):
-            if g[i][j] == 1 and not visited[j]:
-                queue.append(j)
-                visited[j] = 1
+print(edges)
+edges.sort(reverse=True)
+print(edges)
 
-visited_dfs = [0 for _ in range(len(graph))]
-visited_bfs = [0 for _ in range(len(graph))]
-dfs(graph, 0, visited_dfs)
-print()
-bfs(graph, 6, visited_bfs)
+selected_edges = edges[:]
+
+total_cost = sum(cost for cost,a,b in edges)
+
+for cost,a,b in edges:
+    temp_edges = [(c,x,y) for c,x,y in selected_edges if not (c==cost and x==a and y==b)]
+    if is_connected(temp_edges, v):
+        selected_edges = temp_edges
+        total_cost = total_cost - cost
+        print(f"간선 ({a}----{b}, 가중치 : {cost}) 제거, 현 시점 촘 가중치: ({total_cost})")
+    else:
+        print(f"간선 ({a}----{b}, 가중치 : {cost}) 유지: (제거하면 연결 끊어짐)")
+
+print(f"\n 최소 신장 트리의 총 가중치: {total_cost}")
+
+for cost,a,b in sorted(selected_edges):
+    print(f"{a}----{b}, {cost}")
+
+'''
+6 9
+0 1 10
+0 2 15
+1 2 40
+1 3 11
+1 4 50
+2 3 12
+3 4 20
+3 5 30
+4 5 25
+'''
